5796ee0283de7322a886180c2c8128f0dfea67aa
Tolga Cavusoglu <tolga.cavusoglu.06@gmail.com>
2025-11-16 12:06:27 +0000

    Lora Testing

---
+++ Cansat Testing/SimpleRx_test_2/SimpleRx_test_2.ino
@@ -0,0 +1,36 @@
+/*License: CC 4.0 - Attribution, NonCommercial (by Mitch Davis, github.com/thekakester)
+* https://creativecommons.org/licenses/by-nc/4.0/   (See README for details)*/
+#include <LoraSx1262.h>
+
+LoraSx1262 radio;
+byte receiveBuff[255];
+int flag = 0;
+
+void setup() {
+  Serial.begin(9600);
+  while (!Serial){
+  }
+  Serial.println("Booted");
+  radio.configSetFrequency(868E6);
+  radio.configSetBandwidth();
+  radio.configSetCodingRate(5);
+  radio.configSetSpreadingFactor();
+
+  flag = 1;
+  if (!radio.begin()) { //Initialize the radio
+    Serial.println("Failed to initialize radio");
+    flag = -1;
+  }
+}
+
+void loop() {
+  // Receive a packet over radio
+  int bytesRead = radio.lora_receive_async(receiveBuff, sizeof(receiveBuff));
+
+  if (bytesRead > -1) {
+    //Print the payload out over serial
+    Serial.print("Received: ");
+    Serial.write(receiveBuff,bytesRead);
+    Serial.println(); //Add a newline after printing
+  }
+}
---
+++ Cansat Testing/SimpleTx_test_2/SimpleTx_test_2.ino
@@ -0,0 +1,27 @@
+/*License: CC 4.0 - Attribution, NonCommercial (by Mitch Davis, github.com/thekakester)
+* https://creativecommons.org/licenses/by-nc/4.0/   (See README for details)*/
+#include <LoraSx1262.h>
+
+const char* payload = "Hello world.  This a pretty long payload. We can transmit up to 255 bytes at once, which is pretty neat if you ask me";
+LoraSx1262 radio;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  while (!Serial){ 
+  }
+  Serial.println("Booted");
+  radio.configSetFrequency(868E6);
+
+  if (!radio.begin()) { //Initialize radio
+    Serial.println("Failed to initialize radio.");
+  }
+}
+
+void loop() {
+  Serial.print("Transmitting... ");
+  radio.transmit((byte *) payload,strlen(payload));
+  Serial.println("Done!");
+
+  delay(1000);
+}
